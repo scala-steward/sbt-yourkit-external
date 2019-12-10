52d7fb477ec385598932adad83fe825c8dba54c9
Dominic Scheirlinck <dominic@vitaler.com>
2019-12-10 16:30:55 +1300

    Include default value

--- src/main/scala/co/vitaler/sbt/yourkit/YourKit.scala
+++ src/main/scala/co/vitaler/sbt/yourkit/YourKit.scala
@@ -27,6 +27,7 @@ object YourKit extends AutoPlugin {
 
   override lazy val globalSettings: Seq[Def.Setting[_]] = Seq(
     // Guess platform based on os.name, assuming 64-bit
+    yourKitEnabled := true,
     yourKitAgentPlatform := {
       System.getProperty("os.name").toLowerCase match {
         case mac if mac.contains("mac")       => "mac"
